ee6490f25e198c1de1639de692af6776ea0a2291
danielmc99 <dc.camargo09@gmail.com>
2022-04-21 22:17:48 -0500

    Update marvel.js
    correct

--- src/components/marvel.js
+++ src/components/marvel.js
@@ -1,26 +1,30 @@
 import { useEffect, useState } from "react";
 
 function marvel(){
-    const [character, setCharacter] = useState("");
+    const [heroes, setCharacter] = useState("");
     useEffect(()=>{
         if(!navigator.onLine){
-            if(localStorage.getItem("character") === null) {
-                setJoke("Loading...")
+            if(localStorage.getItem("heroes") === null) {
+                setHero("Loading...")
             } else {
-                setJoke(localStorage.getItem("character"));
+                setHero(localStorage.getItem("heroes"));
             }
         } else {
             const URL = "http://gateway.marvel.com/v1/public/comics?ts=1&apikey=3989e5948f21ca214366c6b47642f6eb&hash=e75218f442168db862ce36433bf006f4";
             fetch(URL).then(res=>res.json()).then(res=>{
-                setJoke(res.value);
-                localStorage.setItem("character", res.value);
+                setHero(res.value);
+                localStorage.setItem("heroes", res.value);
             })
         }
     }, []);
 
-    return(
-        <h2>{joke}</h2>
-    )
+    return (
+        <div>
+          <div className="container">
+            <h1>Personajes de Marvel</h1>
+          </div>
+        </div>
+      );
 }
 
-export default Joke;
+export default App;
